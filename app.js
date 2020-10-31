f647f66c57c955a9066026630246a19479ecdb1e
Stefan Constantin-Buliga <stefan@Stefans-MacBook-Pro.local>
2020-10-31 18:22:58 +0000

    Pula

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const express = require('express');
 
 const darkweatherKey = 'c357b87fd02d491e9a4466d4cfd76fde';
 const darkweatherUrl = 'http://api.weatherstack.com/current?access_key=c357b87fd02d491e9a4466d4cfd76fde&query=Vaslui';
@@ -7,8 +8,12 @@ const darkweatherUrl = 'http://api.weatherstack.com/current?access_key=c357b87fd
 const mapboxKey = 'pk.eyJ1IjoiZmFuYWNoZTk5IiwiYSI6ImNrZ3F6cGFldDFyeTAycnJ4Y2RjM21rM2MifQ.KFXwjcaFlHgWumVbNRJZ6Q';
 const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Barbu%20Mumuleanu.json?access_token=pk.eyJ1IjoiZmFuYWNoZTk5IiwiYSI6ImNrZ3F6cGFldDFyeTAycnJ4Y2RjM21rM2MifQ.KFXwjcaFlHgWumVbNRJZ6Q';
 
+const app = express();
 
-request({ url: mapboxUrl }, (error, response) => {
-    let data = JSON.parse(response.body);
-    console.log(data.features[0]);
+app.get('', (req, res) => {
+    res.send({
+        body:"Test",
+    });
 })
+
+app.listen(3000);
